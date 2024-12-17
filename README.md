def4dce9fda7c623d95096be2c10020697bafc06
dirkarnez <smalldirkalex@gmail.com>
2024-12-17 23:01:14 +0800

    - working

--- README.md
+++ README.md
@@ -2,7 +2,12 @@ dawn-playground
 ===============
 ![](https://github.com/dirkarnez/dawn-playground/actions/workflows/build.yml/badge.svg)
 
-Based on [beaufortfrancois/webgpu-cross-platform-app: WebGPU cross-platform app with CMake/Emscripten](https://github.com/beaufortfrancois/webgpu-cross-platform-app)
+### Based on
+- [beaufortfrancois/webgpu-cross-platform-app: WebGPU cross-platform app with CMake/Emscripten](https://github.com/beaufortfrancois/webgpu-cross-platform-app)
+- https://dawn.googlesource.com/dawn/+/refs/heads/main/src/dawn/samples/DawnInfo.cpp
+
+### Notes
+- Needs `dawn-cmake-installation\bin\webgpu_dawn.dll` to run
 
 ### Using
 - [dirkarnez/dawn-prebuilt](https://github.com/dirkarnez/dawn-prebuilt)
--- src/main.cpp
+++ src/main.cpp
@@ -1,171 +1,246 @@
-// #include <GLFW/glfw3.h>
-#include "webgpu/webgpu_cpp.h"
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
-// #if defined(__EMSCRIPTEN__)
-// #include "emscripten/emscripten.h"
-// #else
-// #include "webgpu/webgpu_glfw.h"
-// #endif
-
-wgpu::Instance instance;
-wgpu::Adapter adapter;
-wgpu::Device device;
-wgpu::RenderPipeline pipeline;
-
-wgpu::Surface surface;
-wgpu::TextureFormat format;
-const uint32_t kWidth = 512;
-const uint32_t kHeight = 512;
-
-void ConfigureSurface() {
-  wgpu::SurfaceCapabilities capabilities;
-  surface.GetCapabilities(adapter, &capabilities);
-  format = capabilities.formats[0];
-
-  wgpu::SurfaceConfiguration config{
-      .device = device,
-      .format = format,
-      .width = kWidth,
-      .height = kHeight};
-  surface.Configure(&config);
-}
-
-void GetAdapter(void (*callback)(wgpu::Adapter)) {
-  instance.RequestAdapter(
-      nullptr,
-      // TODO(https://bugs.chromium.org/p/dawn/issues/detail?id=1892): Use
-      // wgpu::RequestAdapterStatus and wgpu::Adapter.
-      [](WGPURequestAdapterStatus status, WGPUAdapter cAdapter,
-         const char* message, void* userdata) {
-        if (message) {
-          printf("RequestAdapter: %s\n", message);
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+// #include "dawn/dawn_proc.h"  // nogncheck
+#include "dawn/native/DawnNative.h"
+#include "dawn/webgpu_cpp_print.h"
+namespace {
+// Wraps a string to about 75 characters and prints indented. Splits on whitespace instead of
+// between characters in a word.
+std::string WrapString(const std::string& in, const std::string& indent) {
+    std::stringstream out;
+    size_t last_space = 0;
+    size_t start_pos = 0;
+    for (size_t i = 0; i < in.size(); ++i) {
+        if (in[i] == ' ') {
+            last_space = i;
+        } else if (in[i] == '\n') {
+            last_space = i;
         }
-        if (status != WGPURequestAdapterStatus_Success) {
-          exit(0);
+        if ((i - start_pos) != 0 && ((i - start_pos) % 75) == 0) {
+            out << indent << in.substr(start_pos, last_space - start_pos) << "\n";
+            start_pos = last_space + 1;
+            last_space = start_pos;
         }
-        wgpu::Adapter adapter = wgpu::Adapter::Acquire(cAdapter);
-        reinterpret_cast<void (*)(wgpu::Adapter)>(userdata)(adapter);
-  }, reinterpret_cast<void*>(callback));
-}
-
-void GetDevice(void (*callback)(wgpu::Device)) {
-  adapter.RequestDevice(
-      nullptr,
-      // TODO(https://bugs.chromium.org/p/dawn/issues/detail?id=1892): Use
-      // wgpu::RequestDeviceStatus and wgpu::Device.
-      [](WGPURequestDeviceStatus status, WGPUDevice cDevice,
-          const char* message, void* userdata) {
-        if (message) {
-          printf("RequestDevice: %s\n", message);
-        }
-        wgpu::Device device = wgpu::Device::Acquire(cDevice);
-        device.SetUncapturedErrorCallback(
-            [](WGPUErrorType type, const char* message, void* userdata) {
-              std::cout << "Error: " << type << " - message: " << message;
-            },
-            nullptr);
-        reinterpret_cast<void (*)(wgpu::Device)>(userdata)(device);
-  }, reinterpret_cast<void*>(callback));
-}
-
-const char shaderCode[] = R"(
-    @vertex fn vertexMain(@builtin(vertex_index) i : u32) ->
-      @builtin(position) vec4f {
-        const pos = array(vec2f(0, 1), vec2f(-1, -1), vec2f(1, -1));
-        return vec4f(pos[i], 0, 1);
     }
-    @fragment fn fragmentMain() -> @location(0) vec4f {
-        return vec4f(1, 0, 0, 1);
+    out << indent << in.substr(start_pos, in.size() - start_pos);
+    return out.str();
+}
+std::string AdapterTypeToString(wgpu::AdapterType type) {
+    switch (type) {
+        case wgpu::AdapterType::DiscreteGPU:
+            return "discrete GPU";
+        case wgpu::AdapterType::IntegratedGPU:
+            return "integrated GPU";
+        case wgpu::AdapterType::CPU:
+            return "CPU";
+        case wgpu::AdapterType::Unknown:
+            break;
     }
-)";
-
-void CreateRenderPipeline() {
-  wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
-  wgslDesc.code = shaderCode;
-
-  wgpu::ShaderModuleDescriptor shaderModuleDescriptor{
-      .nextInChain = &wgslDesc};
-  wgpu::ShaderModule shaderModule =
-      device.CreateShaderModule(&shaderModuleDescriptor);
-
-  wgpu::ColorTargetState colorTargetState{.format = format};
-
-  wgpu::FragmentState fragmentState{.module = shaderModule,
-                                    .targetCount = 1,
-                                    .targets = &colorTargetState};
-
-  wgpu::RenderPipelineDescriptor descriptor{
-      .vertex = {.module = shaderModule},
-      .fragment = &fragmentState};
-  pipeline = device.CreateRenderPipeline(&descriptor);
+    return "unknown";
 }
-
-void Render() {
-  wgpu::SurfaceTexture surfaceTexture;
-  surface.GetCurrentTexture(&surfaceTexture);
-
-  wgpu::RenderPassColorAttachment attachment{
-      .view = surfaceTexture.texture.CreateView(),
-      .loadOp = wgpu::LoadOp::Clear,
-      .storeOp = wgpu::StoreOp::Store};
-
-  wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1,
-                                        .colorAttachments = &attachment};
-
-  wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
-  wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
-  pass.SetPipeline(pipeline);
-  pass.Draw(3);
-  pass.End();
-  wgpu::CommandBuffer commands = encoder.Finish();
-  device.GetQueue().Submit(1, &commands);
+std::string BackendTypeToString(wgpu::BackendType type) {
+    switch (type) {
+        case wgpu::BackendType::Null:
+            return "Null";
+        case wgpu::BackendType::WebGPU:
+            return "WebGPU";
+        case wgpu::BackendType::D3D11:
+            return "D3D11";
+        case wgpu::BackendType::D3D12:
+            return "D3D12";
+        case wgpu::BackendType::Metal:
+            return "Metal";
+        case wgpu::BackendType::Vulkan:
+            return "Vulkan";
+        case wgpu::BackendType::OpenGL:
+            return "OpenGL";
+        case wgpu::BackendType::OpenGLES:
+            return "OpenGLES";
+        case wgpu::BackendType::Undefined:
+            return "Undefined";
+    }
+    return "unknown";
 }
-
-void InitGraphics() {
-  ConfigureSurface();
-  CreateRenderPipeline();
+std::string PowerPreferenceToString(const wgpu::DawnAdapterPropertiesPowerPreference& prop) {
+    switch (prop.powerPreference) {
+        case wgpu::PowerPreference::LowPower:
+            return "low power";
+        case wgpu::PowerPreference::HighPerformance:
+            return "high performance";
+        case wgpu::PowerPreference::Undefined:
+            return "<undefined>";
+    }
+    return "<unknown>";
 }
-
-void Start() {
-  if (!glfwInit()) {
-    return;
-  }
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window =
-      glfwCreateWindow(kWidth, kHeight, "WebGPU window", nullptr, nullptr);
-
-#if defined(__EMSCRIPTEN__)
-  wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
-  canvasDesc.selector = "#canvas";
-
-  wgpu::SurfaceDescriptor surfaceDesc{.nextInChain = &canvasDesc};
-  surface = instance.CreateSurface(&surfaceDesc);
-#else
-  surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
-#endif
-
-  InitGraphics();
-
-#if defined(__EMSCRIPTEN__)
-  emscripten_set_main_loop(Render, 0, false);
-#else
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-    Render();
-    surface.Present();
-    instance.ProcessEvents();
-  }
-#endif
+std::string AsHex(uint32_t val) {
+    std::stringstream hex;
+    hex << "0x" << std::uppercase << std::setfill('0') << std::setw(4) << std::hex << val;
+    return hex.str();
 }
+std::string AdapterInfoToString(const wgpu::AdapterInfo& info) {
+    std::stringstream out;
+    out << "VendorID: " << AsHex(info.vendorID) << "\n";
+    out << "Vendor: " << info.vendor << "\n";
+    out << "Architecture: " << info.architecture << "\n";
+    out << "DeviceID: " << AsHex(info.deviceID) << "\n";
+    out << "Name: " << info.device << "\n";
+    out << "Driver description: " << info.description << "\n";
+    out << "Adapter Type: " << AdapterTypeToString(info.adapterType) << "\n";
+    out << "Backend Type: " << BackendTypeToString(info.backendType) << "\n";
+    return out.str();
+}
+std::string FormatNumber(uint64_t num) {
+    auto s = std::to_string(num);
+    std::stringstream ret;
+    auto remainder = s.length() % 3;
+    ret << s.substr(0, remainder);
+    for (size_t i = remainder; i < s.length(); i += 3) {
+        if (i > 0) {
+            ret << ",";
+        }
+        ret << s.substr(i, 3);
+    }
+    return ret.str();
+}
+std::string LimitsToString(const wgpu::Limits& limits, const std::string& indent) {
+    std::stringstream out;
+    out << indent << "maxTextureDimension1D: " << FormatNumber(limits.maxTextureDimension1D)
+        << "\n";
+    out << indent << "maxTextureDimension2D: " << FormatNumber(limits.maxTextureDimension2D)
+        << "\n";
+    out << indent << "maxTextureDimension3D: " << FormatNumber(limits.maxTextureDimension3D)
+        << "\n";
+    out << indent << "maxTextureArrayLayers: " << FormatNumber(limits.maxTextureArrayLayers)
+        << "\n";
+    out << indent << "maxBindGroups: " << FormatNumber(limits.maxBindGroups) << "\n";
+    out << indent
+        << "maxBindGroupsPlusVertexBuffers: " << FormatNumber(limits.maxBindGroupsPlusVertexBuffers)
+        << "\n";
+    out << indent << "maxBindingsPerBindGroup: " << FormatNumber(limits.maxBindingsPerBindGroup)
+        << "\n";
+    out << indent << "maxDynamicUniformBuffersPerPipelineLayout: "
+        << FormatNumber(limits.maxDynamicUniformBuffersPerPipelineLayout) << "\n";
+    out << indent << "maxDynamicStorageBuffersPerPipelineLayout: "
+        << FormatNumber(limits.maxDynamicStorageBuffersPerPipelineLayout) << "\n";
+    out << indent << "maxSampledTexturesPerShaderStage: "
+        << FormatNumber(limits.maxSampledTexturesPerShaderStage) << "\n";
+    out << indent << "maxSamplersPerShaderStage: " << FormatNumber(limits.maxSamplersPerShaderStage)
+        << "\n";
+    out << indent << "maxStorageBuffersPerShaderStage: "
+        << FormatNumber(limits.maxStorageBuffersPerShaderStage) << "\n";
+    out << indent << "maxStorageTexturesPerShaderStage: "
+        << FormatNumber(limits.maxStorageTexturesPerShaderStage) << "\n";
+    out << indent << "maxUniformBuffersPerShaderStage: "
+        << FormatNumber(limits.maxUniformBuffersPerShaderStage) << "\n";
+    out << indent
+        << "maxUniformBufferBindingSize: " << FormatNumber(limits.maxUniformBufferBindingSize)
+        << "\n";
+    out << indent
+        << "maxStorageBufferBindingSize: " << FormatNumber(limits.maxStorageBufferBindingSize)
+        << "\n";
+    out << indent << "minUniformBufferOffsetAlignment: "
+        << FormatNumber(limits.minUniformBufferOffsetAlignment) << "\n";
+    out << indent << "minStorageBufferOffsetAlignment: "
+        << FormatNumber(limits.minStorageBufferOffsetAlignment) << "\n";
+    out << indent << "maxVertexBuffers: " << FormatNumber(limits.maxVertexBuffers) << "\n";
+    out << indent << "maxBufferSize: " << FormatNumber(limits.maxBufferSize) << "\n";
+    out << indent << "maxVertexAttributes: " << FormatNumber(limits.maxVertexAttributes) << "\n";
+    out << indent
+        << "maxVertexBufferArrayStride: " << FormatNumber(limits.maxVertexBufferArrayStride)
+        << "\n";
+    out << indent
+        << "maxInterStageShaderComponents: " << FormatNumber(limits.maxInterStageShaderComponents)
+        << "\n";
+    out << indent
+        << "maxInterStageShaderVariables: " << FormatNumber(limits.maxInterStageShaderVariables)
+        << "\n";
+    out << indent << "maxColorAttachments: " << FormatNumber(limits.maxColorAttachments) << "\n";
+    out << indent << "maxColorAttachmentBytesPerSample: "
+        << FormatNumber(limits.maxColorAttachmentBytesPerSample) << "\n";
+    out << indent
+        << "maxComputeWorkgroupStorageSize: " << FormatNumber(limits.maxComputeWorkgroupStorageSize)
+        << "\n";
+    out << indent << "maxComputeInvocationsPerWorkgroup: "
+        << FormatNumber(limits.maxComputeInvocationsPerWorkgroup) << "\n";
+    out << indent << "maxComputeWorkgroupSizeX: " << FormatNumber(limits.maxComputeWorkgroupSizeX)
+        << "\n";
+    out << indent << "maxComputeWorkgroupSizeY: " << FormatNumber(limits.maxComputeWorkgroupSizeY)
+        << "\n";
+    out << indent << "maxComputeWorkgroupSizeZ: " << FormatNumber(limits.maxComputeWorkgroupSizeZ)
+        << "\n";
+    out << indent << "maxComputeWorkgroupsPerDimension: "
+        << FormatNumber(limits.maxComputeWorkgroupsPerDimension) << "\n";
+    return out.str();
+}
+void DumpAdapterInfo(const wgpu::Adapter& adapter) {
+    wgpu::DawnAdapterPropertiesPowerPreference power_props{};
+    wgpu::AdapterInfo info{};
+    info.nextInChain = &power_props;
+    adapter.GetInfo(&info);
+    std::cout << AdapterInfoToString(info);
+    std::cout << "Power: " << PowerPreferenceToString(power_props) << "\n";
+    std::cout << "\n";
+}
+void DumpAdapterFeatures(const wgpu::Adapter& adapter) {
+    wgpu::SupportedFeatures supportedFeatures;
+    adapter.GetFeatures(&supportedFeatures);
+    std::cout << "  Features\n";
+    std::cout << "  ========\n";
+    for (uint32_t i = 0; i < supportedFeatures.featureCount; ++i) {
+        wgpu::FeatureName f = supportedFeatures.features[i];
+        auto info = dawn::native::GetFeatureInfo(f);
+        std::cout << "   * " << info->name << "\n";
+        std::cout << WrapString(info->description, "      ") << "\n";
+        std::cout << "      " << info->url << "\n";
+    }
+}
+void DumpAdapterLimits(const wgpu::Adapter& adapter) {
+    wgpu::SupportedLimits adapterLimits;
+    if (adapter.GetLimits(&adapterLimits)) {
+        std::cout << "\n";
+        std::cout << "  Adapter Limits\n";
+        std::cout << "  ==============\n";
+        std::cout << LimitsToString(adapterLimits.limits, "    ") << "\n";
+    }
+}
+void DumpAdapter(const wgpu::Adapter& adapter) {
+    std::cout << "Adapter\n";
+    std::cout << "=======\n";
+    DumpAdapterInfo(adapter);
+    DumpAdapterFeatures(adapter);
+    DumpAdapterLimits(adapter);
+}
+}  // namespace
 
-int main() {
-  instance = wgpu::CreateInstance();
-  GetAdapter([](wgpu::Adapter a) {
-    adapter = a;
-    GetDevice([](wgpu::Device d) {
-      device = d;
-      Start();
+int main(int argc, const char* argv[]) {
+    std::cout << "Hello World\n";
+    
+    auto toggles = dawn::native::AllToggleInfos();
+    std::sort(toggles.begin(), toggles.end(), [](const auto* a, const auto* b) {
+        return std::string(a->name) < std::string(b->name);
     });
-  });
+    std::cout << "Toggles\n";
+    std::cout << "=======\n";
+    bool first = true;
+    for (const auto* info : toggles) {
+        if (!first) {
+            std::cout << "\n";
+        }
+        first = false;
+        std::cout << "  Name: " << info->name << "\n";
+        std::cout << WrapString(info->description, "    ") << "\n";
+        std::cout << "    " << info->url << "\n";
+    }
+    std::cout << "\n";
+    // dawnProcSetProcs(&dawn::native::GetProcs());
+    auto instance = std::make_unique<dawn::native::Instance>();
+    std::vector<dawn::native::Adapter> adapters = instance->EnumerateAdapters();
+    for (const auto& adapter : adapters) {
+        DumpAdapter(wgpu::Adapter(adapter.Get()));
+    }
 }
